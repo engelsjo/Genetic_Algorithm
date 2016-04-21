2276699e065f4be67b86dcbb38a776eccfceb215
Josh <engelsjo@mail.gvsu.edu>
2016-04-21 14:56:58 -0400

    initialization function

--- src/genetic.py
+++ src/genetic.py
@@ -1,12 +1,24 @@
 import os
 import sys
+from random import randint
 
 class GeneticAlgorithm(object):
-    def __init__(self):
+    def __init__(self, initialPopulationSize=100, numChromosomeBits=16):
+        self.initPopulationSize = initialPopulationSize
+        self.nbrOfChromosomeBits = numChromosomeBits
+        self.currentPopulation = []
         self.populationInitialization()
 
+    ############# Evolutionary Process Methods #############
+
     def populationInitialization(self):
-        x = "placeholder"
+        # create as many chromosomes as specified in self.initPopulationSize
+        for chromosomeCounter in range(self.initPopulationSize):
+            chromesome = [] # make a new array which will hold our bits
+            for bitCounter in range(self.nbrOfChromosomeBits):
+                bit = str(randint(0,1))
+                chromesome.append(bit)
+            self.currentPopulation.append(chromesome)
 
     def populationEvaluation(self):
         x = "placeholder"
@@ -29,8 +41,19 @@ class GeneticAlgorithm(object):
             self.populationSelection()
             self.populationVariation()
             self.populationUpdate()
+
+    ############# Helper methods #############
+
+
 def main(argv):
-    ga = GeneticAlgorithm()
+    if len(argv) == 3:
+        initPopulationSize = int(argv[1])
+        numberOfChromosomeBits = int(argv[2])
+        ga = GeneticAlgorithm(initPopulationSize, numberOfChromosomeBits)
 
 if __name__ == "__main__":
     main(sys.argv)
+
+
+
+
